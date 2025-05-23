60d04695611a2f6ec64b2e25cc35d590a71cf494
KARANBIR SINGH <karan0031singh@gmail.com>
2025-05-23 10:48:48 +0530

    rename files and type conversions

---
+++ 3_type_conversion.js
@@ -0,0 +1,19 @@
+let score = "210"
+
+console.log("score "+score)
+console.log(typeof score)
+
+let scoreInNumber = Number(score)
+
+console.log("scoren in number " + scoreInNumber)
+console.log(typeof scoreInNumber)
+
+score = "180abc"
+
+console.log("score "+score)
+console.log(typeof score)
+
+scoreInNumber = Number(score)
+
+console.log("scoren in number " + scoreInNumber)
+console.log(typeof scoreInNumber)
