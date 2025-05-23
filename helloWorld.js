e6dcbbf8f9f564c5178e96ce98a7e22774427afa
KARANBIR SINGH <karan0031singh@gmail.com>
2025-05-23 10:24:11 +0530

    variables using const, var, let, without these

--- helloWorld.js
+++ helloWorld.js
@@ -2,6 +2,25 @@
 console.log("Hello, World!");
 console.log("This is a simple JavaScript program.");
 
-const name = "Karanbir Singh";
-let age = 25
+const userId = 1234
+let userName = "Karanbir Singh"
+var userEmail = "karan@xyz.com"
+userCity = "Amritsar"
+
+
+// prefer let for declaeration of new variable
+
+console.log("user id -" + userId+ ", name - " + userName + ", email -" + userEmail + ", city -" + userCity)
+
+//userId = 1212         // const -- constant
+userName = "Mr singh"
+userEmail = "singh@singh.com"
+userCity = "Chandigarh"
+
+console.log("\n user data ")
+
+console.table([userId, userName, userEmail, userCity])
+
+
+
 
