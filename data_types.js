5449cc27be83277019bec287ada0b5d00c4f594c
KARANBIR SINGH <karan0031singh@gmail.com>
2025-05-23 10:41:40 +0530

    basic data types

--- data_types.js
+++ data_types.js
@@ -1,2 +1,35 @@
 // data types in JS
 
+"use strict"; // treat all JS code as newer version inside this file
+
+
+/**
+ * number
+ * string
+ * boolen
+ * null -- is a object
+ * underfined -- is a data type
+ * 
+ */
+
+console.log(typeof 2156);
+console.log(typeof "karan");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+let age = 22
+let myName = "Karan"
+let stauts = false
+
+let var1 = null
+let var2;
+
+console.log()
+console.log(typeof age);
+console.log(typeof myName);
+console.log(typeof stauts);
+console.log(typeof var1);
+console.log(typeof var2);
+
+
